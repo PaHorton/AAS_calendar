66eae1e2b9f9a0eb1ff356e14e85969466200fd9
Paul Horton <pahorton@asu.edu>
2023-01-08 15:22:24 -0800

    remove js

--- cal_fix.js
+++
@@ -1,8 +0,0 @@
-Dropzone.options.calDrop = { // camelized version of the `id`
-    paramName: "file", // The name that will be used to transfer the file
-    maxFilesize: 2, // MB
-    accept: function(file, done) {
-        console.log("woo")
-        done();
-    }
-};
